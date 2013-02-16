c34c15f01b1c46f272dab8bcce118edb043867f0
edwinmartinez <martinmedia@yahoo.com>
2013-02-16 09:25:18 -0800

    got the initial backbone message working

--- application/views/myhome_view.php
+++ application/views/myhome_view.php
@@ -6,7 +6,6 @@
 		<div class="span2" style="border: 1px solid #ccc;">
 			something here
 			<ul>
-				<div>[picture]</div>
 				
 				<li><?php echo $this->lang->line('common_recent_visits'); ?></li>
 				<li><?php echo $this->lang->line('common_my_photos'); ?></li>
@@ -29,7 +28,12 @@
   		<div class="span10" style="background-color: #CCCCCC;">
   			<h2>Hola, <?php echo $this->session->userdata('username'); ?>!</h2>
   			[the wall]
-  			<div id="main-content">Loading...</div>
+  			
+  			
+  			
+  		<div id="main-content">Loading...</div>
+  		
+  		
   		</div>
 	  
 
@@ -37,9 +41,47 @@
 </div>
 
 
- <script type="text/x-mustache-template" id="index-template">
-      <h2>There are {{ count }} items</h2>
-</script>
+    <script type="text/x-mustache-template" id="index-template">
+      <h2>There are {{ count }} things</h2>
+    </script>
+
+
+<script type="text/x-mustache-template" id="index-original-template">
+      <h2>There are {{ count }} recipes</h2>
+      <div class='recipes'></div>
+    </script>
+    <script type="text/x-mustache-template" id="recipe-template">
+      <h3>{{ name }}</h3>
+      <p>{{ ingredients }}</p>
+      <p>
+        <button class='btn btn-danger'>Delete</button>
+      </p>
+    </script>
+    
+    
+    <script type="text/x-mustache-template" id="form-template">
+      <legend>Add a recipe</legend>
+      <fieldset>
+        <div class='control-group'>
+          <label class='control-label' for='name'>Name</label>
+          <div class='controls'>
+            <input type='text' class='input-xlarge' id='name'>
+          </div>
+        </div>
+        <div class='control-group'>
+          <label class='control-label' for='ingredients'>Ingredients</label>
+          <div class='controls'>
+            <input type='text' class='input-xlarge' id='ingredients'>
+          </div>
+        </div>
+        <div class="form-actions">
+          <button type="submit" class="btn btn-primary">Add Recipe</button>
+        </div>
+      </fieldset>
+    </script>
+
+
+
 <?php 
  if(isset($loadjs)){
  	foreach ($loadjs as $jsscript) {
--- js/myhome.js
+++ js/myhome.js
@@ -1,30 +1,48 @@
 (function() {
   var MyHome = {};
   window.MyHome = MyHome;
-  
-
+	
   var template = function(name) {
     return Mustache.compile($('#'+name+'-template').html());
   };
-  
-   /* 
-   MyHome.Message = Backbone.Model.extend({
+
+  MyHome.Recipe = Backbone.Model.extend({
   });
 
-  MyHome.Messages = Backbone.Collection.extend({
-    localStorage: new Store("messages")
+  MyHome.Recipes = Backbone.Collection.extend({
+    localStorage: new Store("recipes")
   });
-  */
+
   MyHome.Index = Backbone.View.extend({
     template: template('index'),
     initialize: function() {
-      //this.messages = new MyHome.Messages();
-      //this.messages.on('all', this.render, this);
-      //this.messages.fetch();
-      console.log('initialized view');
+      this.recipes = new MyHome.Recipes();
+      this.recipes.on('all', this.render, this);
+      this.recipes.fetch({ success: this.fetchSuccess, error: this.fetchError });
+      //this.fetch({ success: this.fetchSuccess, error: this.fetchError });
+      console.log('init index');
+    },
+    render: function() {
+      this.$el.html(this.template(this));
+      return this;
+    },
+    count: function() {
+      return this.recipes.length;
     }
   });
 
+  /*
+   * To do:
+   *
+   * * MyHome.Index.Form
+   *   A view that renders a form which can be submitted
+   *   to create a new recipe
+   * * MyHome.Index should add a subview for each
+   *   recipe in the database
+   * * MyHome.Recipe
+   *   A view that renders an individual recipe
+   *   Also, a delete button to remove it
+   */
 
   MyHome.Router = Backbone.Router.extend({
     initialize: function(options) {
@@ -34,35 +52,15 @@
       "": "index"
     },
     index: function() {
-    	
       var view = new MyHome.Index();
       this.el.empty();
-      //this.el.append(view.render().el);
-      console.log('index function done');
+      this.el.append(view.render().el);
     }
   });
-
 
   MyHome.boot = function(container) {
     container = $(container);
     var router = new MyHome.Router({el: container})
     Backbone.history.start();
   }
-  
-  /*
-		Member = Backbone.Model.extend({
-
-            defaults : {
-                id : "noid",
-                image : "",
-                username : "",
-                country : "united states"
-            },
-
-            initialize : function Member() {
-                console.log('User Constructor');
-            }
-        });
-*/
-
 })()
